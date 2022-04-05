cd92e9111b57d7a6c84479677aef5bdd0fafde53
liruikai <18332282927@163.com>
2022-04-05 18:30:46 +0800

    first commit

---
+++ app/src/main/java/com/wd/git/Student.java
@@ -0,0 +1,16 @@
+package com.wd.git;/**
+ * 工程名：GIT
+ * 类名：Student
+ */
+/*日期：2022/4/5*/
+/**
+ 类名：Student
+ */
+
+/**
+ 作者：李瑞凯
+ */
+public class Student {
+    private String name;
+    private int age;
+}
